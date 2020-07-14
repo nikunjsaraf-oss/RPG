788b48d2e967806ad4b3f06d0c61a05d672678f5
Nikunj Saraf <nikunjsaraf2001@gmail.com>
2020-07-14 23:01:25 +0530

    Click to Move finish

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -1,20 +1,32 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class Mover : MonoBehaviour
 {
     [SerializeField] Transform target;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+
+
     void Update()
     {
-        GetComponent<NavMeshAgent>().destination = target.position;
+        if (Input.GetMouseButton(0))
+        { 
+            MoveToCursor();
+        }
+
+       }
+
+    private void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);    // Returns a ray going from camera through a screen point.
+        RaycastHit hit;         // Creates a ray starting at origin along direction.
+        bool hasHit = Physics.Raycast(ray, out hit);
+        if (hasHit)
+        {
+            // Debug.DrawRay(lastRay.origin, lastRay.direction * 100);   // Draws a line from start to start + dir in world coordinates.
+            GetComponent<NavMeshAgent>().destination = hit.point;
+
+        }
     }
 }
