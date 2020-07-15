69e48a0d87aae0bff3e03035271acad8c3857dde
Nikunj Saraf <nikunjsaraf2001@gmail.com>
2020-07-15 20:22:21 +0530

    Movement Complete

---
+++ Assets/Scripts/Controller/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{    
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            MoveToCursor();
+        }
+    }
+    private void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);    // Returns a ray going from camera through a screen point.
+        RaycastHit hit;         // Creates a ray starting at origin along direction.
+        bool hasHit = Physics.Raycast(ray, out hit);
+        if (hasHit)
+        {
+            GetComponent<Mover>().MoveTo(hit.point);
+        }
+    }
+}
---
+++ Assets/Scripts/Movement/Mover.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Numerics;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    void Update()
+    {  
+        UpdateAnimator();
+    }
+
+    private void UpdateAnimator()
+    {
+        UnityEngine.Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+        UnityEngine.Vector3 localVelocity = transform.InverseTransformDirection(velocity);      // transform.InverseTransformDirection converts global to variable.
+        float speed = localVelocity.z;
+        GetComponent<Animator>().SetFloat("ForwardSpeed", speed);
+    }
+
+    public void MoveTo(UnityEngine.Vector3 destination)
+    {
+        GetComponent<NavMeshAgent>().destination = destination;
+    }
+}
