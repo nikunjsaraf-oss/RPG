928686967713e1db9cd11cc046a7beb037e4a70b
Nikunj Saraf <nikunjsaraf2001@gmail.com>
2020-07-27 16:13:25 +0530

    fireball with all kinds of impact effect

--- Assets/Scripts/Combat/Projectile.cs
+++ Assets/Scripts/Combat/Projectile.cs
@@ -7,6 +7,7 @@ namespace RPG.Combat
     public class Projectile : MonoBehaviour
     {
         [SerializeField] float projectileSpeed = 1f;
+        [SerializeField] GameObject hitFX = null;
         float damage = 0;
         Health target = null;
 
@@ -43,6 +44,7 @@ namespace RPG.Combat
             if(other.GetComponent<Health>() != target) return;
             if(target.IsDead()) return;
             target.TakeDamage(damage);
+            Instantiate(hitFX, GetAimLocation(), transform.rotation);
             Destroy(gameObject);    
         }
     }
