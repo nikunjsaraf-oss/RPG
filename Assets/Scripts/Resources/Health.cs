bd790b38533cc19f155df1a4c583bcff403b9b22
Nikunj Saraf <nikunjsaraf2001@gmail.com>
2020-07-28 17:58:40 +0530

    Added level and Health

---
+++ Assets/Scripts/Resources/Health.cs
@@ -0,0 +1,60 @@
+﻿using RPG.Stats;
+using RPG.Saving;
+using RPG.Core;
+using UnityEngine;
+
+namespace RPG.Resources
+{
+    public class Health : MonoBehaviour, ISaveable
+    {
+        [SerializeField] float health = 100f;
+
+        bool isDead = false;
+
+        private void Start() 
+        {
+            health = GetComponent<BaseStats>().GetHealth();    
+        }
+
+        public bool IsDead()
+        {
+            return isDead;
+        }
+
+       
+
+        public void TakeDamage(float damage)
+        {
+            health = Mathf.Max(health - damage, 0);
+            if(health == 0)
+            {
+                Die();
+            }
+        }
+
+        private void Die()
+        {
+            if (isDead) return;
+
+            isDead = true;
+            GetComponent<Animator>().SetTrigger("dead");
+            GetComponent<ActionScheduler>().CancelAction();
+        }
+
+        public object CaptureState()
+        {
+            return health;
+        }
+
+        public void RestoreState(object state)
+        {
+            health = (float)state;
+
+            if(health == 0)
+            {
+                Die();
+            }
+        }
+    }
+
+}
