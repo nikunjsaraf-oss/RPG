d5d7391013191b30d6d7414c9408ab129a256c23
Nikunj Saraf <nikunjsaraf2001@gmail.com>
2020-08-04 18:41:28 +0530

    !FaderFixed

--- Assets/Scripts/Scene Management/Fader.cs
+++ Assets/Scripts/Scene Management/Fader.cs
@@ -1,13 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
-using System;
-
 namespace RPG.SceneManagement
 {
     public class Fader : MonoBehaviour
     {
         CanvasGroup canvasGroup;
-        Coroutine coroutine = null;
 
         private void Awake()
         {
@@ -22,29 +19,18 @@ namespace RPG.SceneManagement
 
         public IEnumerator FadeOut(float time)
         {
-           return Fade(1, time);
+            while (canvasGroup.alpha < 1)
+            {
+                canvasGroup.alpha += Time.deltaTime / time;
+                yield return null;
+            }
         }
 
         public IEnumerator FadeIn(float time)
         {
-            return Fade(0,time);
-        }
-
-         private IEnumerator Fade(float target, float time)
-        {
-            if(coroutine != null)
-           {
-               StopCoroutine(coroutine);
-           }
-           coroutine = StartCoroutine(FadeRoutine(target, time));
-           yield return coroutine;
-        }
-
-        private IEnumerator FadeRoutine(float target, float time)
-        {
-             while (Mathf.Approximately(canvasGroup.alpha, target))
+            while (canvasGroup.alpha > 0)
             {
-                canvasGroup.alpha = Mathf.MoveTowards(canvasGroup.alpha, target, Time.deltaTime / time); 
+                canvasGroup.alpha -= Time.deltaTime / time;
                 yield return null;
             }
         }
